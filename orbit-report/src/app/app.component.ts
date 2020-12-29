fed0e76988f8390a0b1698002b8dfa3b6ff20a0a
monnieco <mrcinvestments1@gmail.com>
2020-12-28 23:26:51 -0600

    I am finish

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { bindCallback } from 'rxjs';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];
+  displayList: Satellite[];
+
+  constructor() {
+    this.sourceList =[];
+    this.displayList = [];
+    let satellitesUrl = "https://handlers.education.launchcode.org/static/satellites.json"
+    window.fetch(satellitesUrl).then(function(response) {
+      response.json().then(function(data) {
+
+        let fetchedSatellites = data.satellites;
+        for(let i = 0; i < fetchedSatellites.lenght; i++){
+          let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].counts);
+
+          this.sourceList.push(satellite);
+          this.displayList =this.sourceList.slice(0);
+
+        }
+
+       }.bind(this)); 
+      }.bind(this));
 }
+search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i = 0; i < this. sourceList.length; i++) {
+   let name = this.sourceList[i].name.toLowerCase();
+   if (name.indexof(searchTerm) >= 0) {
+    matchingSatellites.push(this.sourceList[i]); 
+   }
+  
+  }
+  this.displayList = matchingSatellites;
+
+  
+};
